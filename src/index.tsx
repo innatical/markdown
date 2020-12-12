9e3046bd215452c47c2df36a0797047010a97816
Lleyton Gray <lleyton@innatical.com>
2020-12-11 16:13:36 -0800

    Add parseMarkdown function

--- src/index.tsx
+++ src/index.tsx
@@ -10,30 +10,36 @@ const replace = (regex: RegExp, replacement: Replacer) => (
   text: ReactNodeArray
 ) => reactStringReplace(text, regex, replacement)
 
+interface ParserOptions {
+  bold: Replacer
+  italic: Replacer
+  underlined: Replacer
+  strikethough: Replacer
+  link: Replacer
+  codeblock: Replacer
+  custom: [RegExp, Replacer][]
+}
+
+export const parseMarkdown = (
+  text: string,
+  options: ParserOptions
+) => pipe<ReactNodeArray>(
+  [text],
+  replace(/\*\*(.*)\*\*/gim, options.bold),
+  replace(/\*(.*)\*/gim, options.italic),
+  replace(/__(.*)__/gim, options.underlined),
+  replace(/~~(.*)~~/gim, options.strikethough),
+  replace(/(https?:\/\/[^\s$.?#].[^\s]*)/gim, options.link),
+  replace(/```((.|\n)*)```/gim, options.codeblock),
+  ...options.custom.map((custom) => replace(custom[0], custom[1]))
+)
+
 const useMarkdown = (
   text: string,
-  options: {
-    bold: Replacer
-    italic: Replacer
-    underlined: Replacer
-    strikethough: Replacer
-    link: Replacer
-    codeblock: Replacer
-    custom: [RegExp, Replacer][]
-  }
+  options: ParserOptions
 ) => {
   return useMemo(
-    () =>
-      pipe<ReactNodeArray>(
-        [text],
-        replace(/\*\*(.*)\*\*/gim, options.bold),
-        replace(/\*(.*)\*/gim, options.italic),
-        replace(/__(.*)__/gim, options.underlined),
-        replace(/~~(.*)~~/gim, options.strikethough),
-        replace(/(https?:\/\/[^\s$.?#].[^\s]*)/gim, options.link),
-        replace(/```((.|\n)*)```/gim, options.codeblock),
-        ...options.custom.map((custom) => replace(custom[0], custom[1]))
-      ),
+    () => parseMarkdown(text, options),
     [text, options]
   )
 }
