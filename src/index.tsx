323738206b4bc5c8497a644e64a9fdc646ed6992
Lleyton Gray <lleyton@innatical.com>
2021-05-19 17:47:14 -0700

    owo

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,9 @@ type Replacer = (match: string, index: number) => React.ReactNode
 const pipe = <T extends any>(input: T, ...functions: ((input: T) => T)[]) =>
   functions.reduce((prev, func) => func(prev), input)
 
-const replace = (regex: RegExp, replacement: Replacer) => (
-  text: ReactNodeArray
-) => reactStringReplace(text, regex, replacement)
+const replace =
+  (regex: RegExp, replacement: Replacer) => (text: ReactNodeArray) =>
+    reactStringReplace(text, regex, replacement)
 
 interface ParserOptions {
   bold: Replacer
@@ -25,13 +25,13 @@ type Input = string | ReactNodeArray
 export const parseMarkdown = (text: Input, options: ParserOptions) =>
   pipe<ReactNodeArray>(
     [text],
-    replace(/(https?:\/\/[^\s$.?#].[^\s]*)/gim, options.link),
-    replace(/\*\*(.*?)\*\*/gim, options.bold),
-    replace(/\*(.*?)\*/gim, options.italic),
-    replace(/__(.*?)__/gim, options.underlined),
-    replace(/_(.*?)_/gim, options.italic),
-    replace(/~~(.*?)~~/gim, options.strikethough),
-    replace(/```((.|\n)*)```/gim, options.codeblock),
+    replace(/(https?:\/\/[^\s$.?#].[^\s]+)/gim, options.link),
+    replace(/\*\*(.+?)\*\*/gim, options.bold),
+    replace(/\*(.+?)\*/gim, options.italic),
+    replace(/__(.+?)__/gim, options.underlined),
+    replace(/_(.+?)_/gim, options.italic),
+    replace(/~~(.+?)~~/gim, options.strikethough),
+    replace(/```([^]+?)```/gim, options.codeblock),
     ...options.custom.map((custom) => replace(custom[0], custom[1]))
   )
 
